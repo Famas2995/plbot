9235d3e54978cd6d8a8ef06f17b2e691f760e236
b1tt0 <b1tt0@mail.ru>
2021-01-26 23:01:49 +0300

    ping

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 const { Client } = require("discord.js");
 const bot = new Client();
+const config = require("./config.json");
+
+// im testing shit out so dont mind the bad code
+bot.once("ready", () => console.log("logged in"));
+bot.on("message", (msg) => {
+  if (!msg.content.startsWith(config.prefix))
+    return;
+
+  let args = msg.slice(config.prefix.length).split(" ");
+  let cmd = args[0].toLowerCase();
+
+  if (cmd == "ping") {
+    msg.reply("pong");
+  }
+});
 
 bot.login(process.env.TOKEN);
