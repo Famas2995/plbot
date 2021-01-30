27f9aa76f7ca6d3e76c0a3dddd0346c8a05bd405
b1tt0 <thssthfx@gmail.com>
2021-01-30 23:35:02 +0300

    yeah

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,9 @@
-const os = require("os"); // for ram usage info
-
 module.exports = {
   name: "ping",
   desc: "display a lot of information about the bot",
   run: (msg, args, bot) => {
+    let mem = process.memoryUsage().heapUsed / 1024 / 1024;
     // very messy but idc lolll
-    msg.reply(`${Date.now() - msg.createdTimestamp}ms\n\n**nerdy info**\nfree mem: ${os.freemem()}\nuptime: ${process.uptime()}\nnodejs version:${process.version}`);
+    msg.reply(`${Date.now() - msg.createdTimestamp}ms\n\n**nerdy info**\nused ${Math.round(mem*100)/100}mb\nnodejs version:${process.version}`);
   }
 }
