b85079511c037215a1ba6774a724ab2b7f1c2ece
b1tt0 <b1tt0@mail.ru>
2021-01-30 19:57:55 +0300

    hotfix

--- commands/rules.js
+++ commands/rules.js
@@ -5,7 +5,7 @@ module.exports = {
   desc: "shows you server rules or a specific rule",
   run: (msg, args) => {
     // if there's a rule number provided
-    if (args[2]) msg.reply(rules[args[2]--]);
+    if (args[1]) msg.reply(rules[args[2]--]);
     // just show the rules
     else msg.reply(rules.join("\n\n"));
   }
