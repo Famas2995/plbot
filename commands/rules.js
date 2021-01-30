a4d4cae9f7baccb74a22611e07d608793c15418d
b1tt0 <b1tt0@mail.ru>
2021-01-30 19:53:38 +0300

    rules

---
+++ commands/rules.js
@@ -0,0 +1,12 @@
+const { rules } = require("../util/rules.json");
+
+module.exports = {
+  name: "rules",
+  desc: "shows you server rules or a specific rule",
+  run: (msg, args) => {
+    // if there's a rule number provided
+    if (args[2]) msg.reply(rules[args[2]--]);
+    // just show the rules
+    else msg.reply(rules.join("\n\n"));
+  }
+}
