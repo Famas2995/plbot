b509da9ef26f1f269300dcc323aa1d6aca99fa38
b1tt0 <thssthfx@gmail.com>
2021-01-31 10:57:16 +0300

    attempt 5.. or 6 idk

--- main.ts
+++ main.ts
@@ -2,18 +2,40 @@
 import { createRequire } from "https://deno.land/std/node/module.ts";
 const require = createRequire(import.meta.url);
 
-const { readdirSync } = require("fs");
-const { Client,Collection } = require("discord.js");
-const bot = new Client();
-bot.cmds = new Collection();
-const config = require("./config.json");
+// dependencies
+import Discord from "https://cdn.skypack.dev/pin/discord.js@v12.5.1-zBMhEZtbJvzdMr78PlKZ/min/discordjs.js";
 
-// im testing shit out so dont mind the bad code
+// my own types
+import { Command } from "./types/command.ts"
+
+// variables
+const bot: any = new Discord.Client();
+bot.cmds = new Discord.Collection();
+let commands: RBTree<Command> = new RBTree();
+const config: any = require("./config.json");
+
+/*
+startBot({
+  token: process.env.TOKEN,
+  intents: ["GUILD_MESSAGES"],
+  eventHandlers: {
+    ready: () => {
+      let files = readdirSync(config.cmds).filter((file: string) => file.endsWith(".js"));
+      for (let file of files) {
+        let c = require(`${config.cmds}/${file}`);
+        commands.insert({name: c.name, desc: c.desc, run: c.run});
+      }
+
+      bot.
+    }
+  }
+})*/
+
 bot.once("ready", () => {
   let files = readdirSync(config.cmds).filter((file: string) => file.endsWith(".js"));
   for (let file of files) {
-    let c = require(`${config.cmds}/${file}`);
-    bot.cmds.set(c.name, c);
+    let c: Command = require(`${config.cmds}/${file}`);
+    bot.cmds.set(c.name, c)
   }
 
   bot.user.setActivity("PAHC's Lounge | p!", { type: "LISTENING" });
@@ -24,11 +46,11 @@ bot.on("message", (msg: any) => {
   if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
 
   // split the message into arguments aka keywords
-  let args = msg.content.slice(config.prefix.length).trim().split(" ");
-  let cmd = args[0].toLowerCase();
+  let args: string[] = msg.content.slice(config.prefix.length).trim().split(" ");
+  let cmd: string = args[0].toLowerCase();
 
   try {
-    let toRun = bot.cmds.get(cmd);
+    let toRun: Command = bot.cmds.get(cmd);
     if (!toRun) {
       return msg.reply("command not found!");
     } else {
---
+++ types/command.ts
@@ -0,0 +1,5 @@
+export interface Command = {
+  name: string;
+  desc: string;
+  run: (msg: any, args: string[]) => void;
+}
