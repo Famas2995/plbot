f94e09fb8977cd30da495458623a205ed6cf5fdf
b1tt0 <thssthfx@gmail.com>
2021-01-31 14:33:01 +0300

    variables

--- events/ready.js
+++ events/ready.js
@@ -3,11 +3,14 @@ const { readdirSync } = require("fs");
 const config = require("../config.json");
 
 module.exports = (bot) => {
+  // get all files with js code
   let files = readdirSync(`../${config.cmds}`).filter(file => file.endsWith(".js"));
     for (let file of files) {
+      // require them and add to commands
       let c = require(`../${config.cmds}/${file}`);
       bot.cmds.set(c.name, c);
     }
-  
+
+    // fancy status
     bot.user.setActivity("PAHC's Lounge | p!help", { type: "LISTENING" });
 }
