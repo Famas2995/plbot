aa6b7e083bf134887d672f2397cdb5bb7a0c929a
b1tt0 <thssthfx@gmail.com>
2021-01-31 12:19:58 +0300

    procfile fix + eval command

---
+++ commands/eval.js
@@ -0,0 +1,17 @@
+const { owners } = require("../config.json");
+
+module.exports = {
+  name: "eval",
+  desc: "evaluate code on the server (very sneaky)",
+  run: (msg, args, bot) => {
+    // check if its sent by a bot owner
+    let allowed = false;
+    owners.forEach((owner) => {
+      if (msg.author.id == owner) allowed = true;
+    });
+    if (!allowed) return;
+
+    let code = args.shift().join(" ");
+    msg.reply(eval(code));
+  }
+}
