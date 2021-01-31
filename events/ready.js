1d2b6d4ad0376ebf0f33fb871bbffe3ecb47a567
b1tt0 <thssthfx@gmail.com>
2021-01-31 15:02:08 +0300

    hi

--- events/ready.js
+++ events/ready.js
@@ -5,12 +5,16 @@ const config = require("../config.json");
 module.exports = (bot) => {
   // get all files with js code
   let files = readdirSync(`./${config.cmds}`).filter(file => file.endsWith(".js"));
-    for (let file of files) {
-      // require them and add to commands
-      let c = require(`../${config.cmds}/${file}`);
-      bot.cmds.set(c.name, c);
-    }
+  for (let file of files) {
+    // require them and add to commands
+    let c = require(`../${config.cmds}/${file}`);
+    bot.cmds.set(c.name, c);
+  }
 
-    // fancy status
-    bot.user.setActivity("PAHC's Lounge | p!help", { type: "LISTENING" });
+  // fetch all members from pahcs lounge
+  guild.members.fetch("763475376428154920")
+    .then().catch(console.error);
+
+  // fancy status
+  bot.user.setActivity("PAHC's Lounge | p!help", { type: "LISTENING" });
 }
