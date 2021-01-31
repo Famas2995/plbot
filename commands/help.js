9b0f729e189da22897178bef09e68dc03bdc00be
b1tt0 <thssthfx@gmail.com>
2021-01-31 14:12:40 +0300

    all commands in help

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,11 @@ module.exports = {
   desc: "show all commands or info on a specific commamd",
   run: async (msg, args, bot) => {
     let cmd = bot.cmds.get(args[1]);
-    msg.reply(`**${cmd.name}**: ${cmd.desc}`);
+    if (cmd) return msg.reply(`**${cmd.name}**: ${cmd.desc}`);
+    // list all commands
+    let full = "";
+    bot.cmds.forEach((cmd) => full += `${cmd} `);
+
+    msg.reply(full);
   }
 }
