a894e7806e40af96da0694e06d7f272518163af4
b1tt0 <thssthfx@gmail.com>
2021-02-02 23:17:46 +0300

    client options

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 // events
-const onReady = require("./events/ready");
+const onReady   = require("./events/ready");
 const onMessage = require("./events/message")
 
 // dependencies
 const { Client,Collection } = require("discord.js");
-const bot = new Client();
+const bot = new Client({
+  // cache n shit
+  messageCacheMaxSize      : 10,
+  messageCacheLifetime     : 5,
+  messageSweepInterval     : 5,
+  messageEditHistoryMaxSize: 1,
+
+  disableMentions: "everyone",
+
+  retryLimit: 3,
+
+  ws: {
+    intents: ["GUILD_MESSAGES"]
+  }
+});
 bot.cmds = new Collection();
 const config = require("./config.json");
 
 bot.once("ready", () => onReady(bot));
-bot.on("message", async (msg) => onMessage(msg, bot));
+bot.on("message", async msg => onMessage(msg, bot));
 
 bot.login(process.env.TOKEN);
